89bd95a66aa9159eb11de1c31290a4a7aa1b20e0
sribalaji0110 <sri.balaji@doodleblue.com>
2022-06-17 22:37:23 +0530

    build updtae

---
+++ build/precache-manifest.4548cf1df99f05188336eea176059b23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d137b109fe2e2fc0924e3fecf7988277",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d6e53a7c25a03778fd1",
+    "url": "/static/css/2.7374898e.chunk.css"
+  },
+  {
+    "revision": "f613c32e2295f8737ac0",
+    "url": "/static/css/main.1de34a35.chunk.css"
+  },
+  {
+    "revision": "1d6e53a7c25a03778fd1",
+    "url": "/static/js/2.a4a85815.chunk.js"
+  },
+  {
+    "revision": "f613c32e2295f8737ac0",
+    "url": "/static/js/main.0ff03c4e.chunk.js"
+  },
+  {
+    "revision": "7a323bdebef89b18c197",
+    "url": "/static/js/runtime-main.a4cc3a36.js"
+  }
+]);
