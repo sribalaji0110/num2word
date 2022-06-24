4d476598c954bfb4c43d9d2f45970d38dfdd8c65
sribalaji0110 <sribalaji0110@gmail.com>
2022-06-24 23:01:29 +0530

    latest code

--- sitemap-builder.js
+++ sitemap-builder.js
@@ -1,10 +1,8 @@
 require("babel-register")({
-  presets: ["es2015", "react"]
+  presets: ["es2015", "react"],
 });
-const router = require('./routes').default;
-const Sitemap = require('react-router-sitemap').default;
-(
-	new Sitemap(router)
-    .build("https://numberstowords.online")
-		.save('./public/sitemap.xml')
-);
+const router = require("./routes").default;
+const Sitemap = require("react-router-sitemap").default;
+new Sitemap(router)
+  .build("https://www.numberstowords.online/")
+  .save("./public/sitemap.xml");
